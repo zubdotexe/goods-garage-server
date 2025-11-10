ab95c2116d6b638993b604db394ef6d16e1f3ccd
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-11-10 18:39:02 +0600

    added CORS and express.json() middleware for handling cross-origin requests and JSON payloads

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("GoodsGarage server is running!");
+    res.send({ message: "GoodsGarage server is running!" });
 });
 
 app.listen(port, () => {
